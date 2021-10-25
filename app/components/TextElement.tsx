1266a772aab6f6855c1eb43803f261e5ddfd54f0
Snigdha Singh <snigdha920@gmail.com>
2021-10-25 18:53:19 +0530

    add prop children for TextElement

--- app/components/TextElement.tsx
+++ app/components/TextElement.tsx
@@ -5,17 +5,19 @@ type TextStyles = 'h1' | 'h2' | 'h3' | 'body1' | 'body2';
 
 interface TextElementProps {
   as: TextStyles;
+  children: string;
 }
 
-export const TextElement: React.FC<TextElementProps> = ({as}: TextElementProps) => {
-  const styles: {[key in TextStyles]: string} = {
+export const TextElement: React.FC<TextElementProps> = ({ as, children }: TextElementProps) => {
+  const styles: { [key in TextStyles]: string } = {
     'h1': '',
     'h2': '',
     'h3': '',
     'body1': '',
     'body2': ''
-  }
+  };
   const style = styles[as];
-  return <p className={`${style}`}>
-  </p>;
+  return (
+    <p className={`${style}`}>{children}
+    </p>);
 };
