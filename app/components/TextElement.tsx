d0f1a643fa5298f3832e3f48b13197dd077da914
Snigdha Singh <snigdha920@gmail.com>
2021-10-25 18:44:29 +0530

    set up interface and props for TextElement

--- app/components/TextElement.tsx
+++ app/components/TextElement.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
-export const TextElement = () => {
-  return <div>
-  </div>;
+
+type TextStyles = 'h1' | 'h2' | 'h3' | 'body1' | 'body2';
+
+interface TextElementProps {
+  as: TextStyles;
+}
+
+export const TextElement: React.FC<TextElementProps> = ({as}: TextElementProps) => {
+  const styles: {[key in TextStyles]: string} = {
+    'h1': '',
+    'h2': '',
+    'h3': '',
+    'body1': '',
+    'body2': ''
+  }
+  const style = styles[as];
+  return <p className={`${style}`}>
+  </p>;
 };
