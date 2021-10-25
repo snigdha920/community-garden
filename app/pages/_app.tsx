71763eb145b1f51260f95a698c72052dda352062
Snigdha Singh <snigdha920@gmail.com>
2021-10-25 19:00:44 +0530

    add react-bootstrap and integrate the CSS styles

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossOrigin="anonymous"
+    />
+
+    <Component {...pageProps} /></>;
 }
-export default MyApp
+export default MyApp;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div>Testing bootstrap</div>
 
     </div>
   );
