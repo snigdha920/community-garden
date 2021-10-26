02e19da2e9af609a80b11a8c8ee67605dd510267
Snigdha Singh <snigdha920@gmail.com>
2021-10-26 15:41:41 +0530

    Change color of NavBar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,49 +3,51 @@ import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 
 export const NavBar: React.FC = () => {
+  const navItems: Array<{ label: string; href: string }> = [
+    {
+      label: 'Home',
+      href: '#',
+    },
+    {
+      label: 'My community',
+      href: '/places',
+    },
+    {
+      label: 'About',
+      href: '#AboutTheProjectSection',
+    },
+    {
+      label: 'Contact us',
+      href: '#ContactUsSection',
+    },
+  ];
   return (
     <>
       <Navbar
         fixed="top"
         style={{
-          padding: 2,
           height: '60px',
-          backgroundImage: 'linear-gradient(to right, #1CD8D2 0%, #93EDC7  51%, #1CD8D2  100%)',
+          backgroundImage: 'linear-gradient(to right, #1CD8D2 0%,  #93EDC7  51%)',
+          padding: '0 40px',
         }}
+        className="btn-grad"
       >
-        <div style={{ marginLeft: 20 }}>
-          <Navbar.Brand href="#home">
+        <div>
+          <Navbar.Brand href="/">
             <img src="/icons/sapling.svg" width="30" height="30" className="d-inline-block align-top" alt="logo" />
           </Navbar.Brand>
         </div>
-        {/* Figure out the font */}
-        <Nav style={{ marginLeft: 'auto', marginRight: 15 }}>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/places">My Community</Nav.Link>
-          <Nav.Link href="#AboutTheProjectSection">About</Nav.Link>
-          <Nav.Link href="#">Contact us</Nav.Link>
+
+        <Nav style={{ marginLeft: 'auto' }}>
+          {navItems.map((item, index) => {
+            return (
+              <Nav.Link key={index} href={item.href} style={{ paddingLeft: '30px' }}>
+                {item.label}
+              </Nav.Link>
+            );
+          })}
         </Nav>
       </Navbar>
     </>
   );
 };
-
-//  .btn-grad {background-image: linear-gradient(to right, #1CD8D2 0%, #93EDC7  51%, #1CD8D2  100%)}
-//  .btn-grad {
-//     margin: 10px;
-//     padding: 15px 45px;
-//     text-align: center;
-//     text-transform: uppercase;
-//     transition: 0.5s;
-//     background-size: 200% auto;
-//     color: white;
-//     box-shadow: 0 0 20px #eee;
-//     border-radius: 10px;
-//     display: block;
-//   }
-
-//   .btn-grad:hover {
-//     background-position: right center; /* change the direction of the change here */
-//     color: #fff;
-//     text-decoration: none;
-//   }
