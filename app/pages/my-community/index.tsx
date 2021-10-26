f1a561de836644948be97e420cbc55126e1200f1
Snigdha Singh <snigdha920@gmail.com>
2021-10-26 18:50:38 +0530

    set up My Community page

--- app/pages/my-community/index.tsx
+++ app/pages/my-community/index.tsx
@@ -5,11 +5,25 @@ import { AppLayout } from '../../components/layout/AppLayout';
 import { ContentLayout } from '../../components/layout/ContentLayout';
 
 const Places: NextPage = () => {
+  const places = ['Pomona', 'Yorba Linda', 'Chino Hills'];
   return (
     <AppLayout>
       <>
-        <ContentLayout title="Places" form={<div>Dummy form contents</div>}>
-          <ButtonElement variant="border">Dummy</ButtonElement>
+        <ContentLayout title="My Community" form={<div>Dummy form contents</div>}>
+          <div>
+            <p className="m-4 mb-0">
+              Select your community location to view and monitor the health of your plants. <br />
+              If you would like to have your own community garden, our project is open source and we can help with the
+              installation process.
+            </p>
+            <div className="d-flex flex-wrap">
+              {places.map((place, index) => (
+                <ButtonElement key={index} variant="border" className="m-4">
+                  {place}
+                </ButtonElement>
+              ))}
+            </div>
+          </div>
         </ContentLayout>
       </>
     </AppLayout>
