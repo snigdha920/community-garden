928cc21eba4a9930a5fa118276f55e2011c89ba4
Snigdha Singh <snigdha920@gmail.com>
2021-10-27 03:07:05 +0530

    make NavBar responsive and use bootstrap classes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,7 +9,7 @@ export const NavBar: React.FC = () => {
       href: '/',
     },
     {
-      label: 'My community',
+      label: 'My Community',
       href: '/my-community',
     },
     {
@@ -24,30 +24,32 @@ export const NavBar: React.FC = () => {
   return (
     <>
       <Navbar
+        collapseOnSelect
+        expand="lg"
         fixed="top"
         style={{
           height: '60px',
-          backgroundImage: 'linear-gradient(to right, #1CD8D2 0%,  #93EDC7  51%)', // TODO : Fix left side of navbar (z-index)
+          backgroundImage: 'linear-gradient(to right, #1CD8D2 0%,  #93EDC7  51%)',
           padding: '0 40px',
-          zIndex: 10, //Trial fix
+          zIndex: 10,
         }}
         className="btn-grad"
       >
-        <div>
-          <Navbar.Brand href="/">
-            <img src="/icons/sapling.svg" width="30" height="30" className="d-inline-block align-top" alt="logo" />
-          </Navbar.Brand>
-        </div>
-       
-        <Nav style={{ marginLeft: 'auto' }}>
-          {navItems.map((item, index) => {
-            return (
-              <Nav.Link key={index} href={item.href} style={{ paddingLeft: '30px' }}>
-                {item.label}
-              </Nav.Link>
-            );
-          })}
-        </Nav>
+        <Navbar.Brand href="/">
+          <img src="/icons/sapling.svg" width="30" height="30" className="d-inline-block align-top " alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle id="responsive-navbar-nav" style={{ height: '60px', border: 'none' }} />
+        <Navbar.Collapse>
+          <Nav className="ms-auto">
+            {navItems.map((item, index) => {
+              return (
+                <Nav.Link key={index} href={item.href} className="ps-4">
+                  {item.label}
+                </Nav.Link>
+              );
+            })}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
