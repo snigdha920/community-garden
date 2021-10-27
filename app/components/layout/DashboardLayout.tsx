5ae418d56ac6f5abf90fd4852bbbf6120f811950
Snigdha Singh <snigdha920@gmail.com>
2021-10-27 05:31:01 +0530

    use GridLayout in DashboardLayout

--- app/components/layout/DashboardLayout.tsx
+++ app/components/layout/DashboardLayout.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { Styles } from '../../styles/styles';
+import { Col } from 'react-bootstrap';
 import { CardElement } from '../elements/CardElement';
 import { TextIcon } from '../TextIcon';
 import { BoxLayout } from './BoxLayout';
+import { GridLayout } from './GridLayout';
 
 interface DashBoardLayoutProps {
   place: string;
@@ -13,31 +13,13 @@ interface DashBoardLayoutProps {
 export const DashboardLayout: React.FC<DashBoardLayoutProps> = ({ place }) => {
   const locationCharacteristics: Array<{ card: React.ReactElement }> = [
     {
-      card: (
-        <CardElement
-          label="Sunlight"
-          value="Excellent"
-          icon="/icons/sun.png"
-        ></CardElement>
-      ),
+      card: <CardElement label="Sunlight" value="Excellent" icon="/icons/sun.png"></CardElement>,
     },
     {
-      card: (
-        <CardElement
-          label="Temperature"
-          value="30 °C"
-          icon="/icons/temperature.png"
-        ></CardElement>
-      ),
+      card: <CardElement label="Temperature" value="30 °C" icon="/icons/temperature.png"></CardElement>,
     },
     {
-      card: (
-        <CardElement
-          label="Humidity"
-          value="50%"
-          icon="/icons/humidity.png"
-        ></CardElement>
-      ),
+      card: <CardElement label="Humidity" value="50%" icon="/icons/humidity.png"></CardElement>,
     },
   ];
 
@@ -45,7 +27,7 @@ export const DashboardLayout: React.FC<DashBoardLayoutProps> = ({ place }) => {
     <>
       <BoxLayout className="mt-5">
         <>
-          <div style={{ minHeight: '80px'}}>
+          <div style={{ minHeight: '80px' }}>
             <TextIcon
               icon={<img src="/icons/analytics.png" width="20px" height="20px" alt="pin" />}
               className="px-4 pt-3 pb-2"
@@ -56,13 +38,15 @@ export const DashboardLayout: React.FC<DashBoardLayoutProps> = ({ place }) => {
               <b>Data</b>
             </TextIcon>
           </div>
-          <Container className='mb-4'>
-            <Row>
-              {locationCharacteristics.map((item, index) => {
-                return <Col key={index}>{item.card}</Col>;
-              })}
-            </Row>
-          </Container>
+          <GridLayout>
+            {locationCharacteristics.map((item, index) => {
+              return (
+                <Col key={index} className="mb-4">
+                  {item.card}
+                </Col>
+              );
+            })}
+          </GridLayout>
         </>
       </BoxLayout>
     </>
